d643de3cec390d441845127f6819e1b516188940
Stephan Nirschl <skywalkers@posteo.de>
2021-08-18 16:05:09 +0200

    Projektdateien hinzufügen.

---
+++ Mimir_CMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mimir_CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
